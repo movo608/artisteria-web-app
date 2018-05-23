6d23075e69550892acfb36679d771798f7beb23e
Moldovan Andrei <moldovanandrei8399@gmail.com>
2018-05-23 20:00:17 +0300

    Commit
    Commit

--- api/modules/admin/Module.php
+++ api/modules/admin/Module.php
@@ -2,6 +2,9 @@
 
 namespace app\modules\admin;
 
+use Yii;
+use yii\helpers\Url;
+
 /**
  * admin module definition class
  */
@@ -20,5 +23,17 @@ class Module extends \yii\base\Module
         parent::init();
 
         // custom initialization code goes here
+    }
+
+    /**
+     * Before action
+     */
+    public function beforeAction($action)
+    {
+        if (Yii::$app->controller->module->id === 'admin' && Yii::$app->user->isGuest) {
+            Yii::$app->response->redirect(Url::to(['/login']));
+        }
+
+        return parent::beforeAction($action);
     }
 }
