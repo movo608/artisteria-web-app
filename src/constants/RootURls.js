49daf5f8e5b3cb8a46a5659c764fe503fbe9687d
Moldovan Andrei <moldovanandrei8399@gmail.com>
2018-05-23 21:31:48 +0300

    Commit
    Commit

--- src/constants/RootURls.js
+++ src/constants/RootURls.js
@@ -1,4 +1,20 @@
-export const API = 'http://127.0.0.1/artisteria-web-app/api/web/api/core/'
-export const ADMIN = 'http://127.0.0.1/artisteria-web-app/api/web/admin'
-export const HOME = 'localhost:3000'
-export const UPLOADS = 'http://127.0.0.1/artisteria-web-app/api/uploads/'
+/**
+ * Localhost
+ * # uncomment to apply the localhost URL exports
+ */
+/**
+ * export const API = 'http://127.0.0.1/artisteria-web-app/api/web/api/core/'
+ * export const ADMIN = 'http://127.0.0.1/artisteria-web-app/api/web/admin'
+ * export const HOME = 'localhost:3000'
+ * export const UPLOADS = 'http://127.0.0.1/artisteria-web-app/api/uploads/'
+ */
+
+/**
+ * Webhost 
+ * home: www.artisteria.ro
+ * api: www.artisteria.ro/api
+ */
+export const API = 'http://www.artisteria.ro/api/web/api/core/'
+export const ADMIN = 'http://www.artisteria.ro/api/web/admin'
+export const HOME = 'www.artisteria.ro'
+export const UPLOADS = 'http://www.artisteria.ro/api/uploads/'
